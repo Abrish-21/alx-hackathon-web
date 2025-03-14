629f35a212f5623cafa9e6788088acb8173f394e
Abrham Tilksew <abrham.tilksew@aastustudent.edu.et>
2025-03-14 17:14:53 +0300

    edit text

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 
-function Footer() {
+function Footer() { 
   return (
     <footer className="bg-white">
       <div className="max-w-5xl mx-auto py-12 px-4 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
@@ -17,12 +17,12 @@ function Footer() {
         </div>
         <div className="mt-8 md:mt-0 md:order-1">
           <p className="text-center text-base text-gray-400">&copy; {new Date().getFullYear()} Hospitality Hackathon.</p>
-        </div>
+        </div>npm i react-icons
         <div className="mt-8 md:mt-0 md:order-1">
         <p className="text-center text-base text-gray-400">
-            Made with ❤️ By 
+            Made with ❤️ by 
             <a href="https://www.linkedin.com/in/abrham28/" target="_blank" rel="noopener noreferrer" className="text-blue-500">  Abrham Tilksew </a>  | 
-            <a href="https://www.linkedin.com/in/sumeya-akmel-2a3b9b270/" target="_blank" rel="noopener noreferrer" className="text-blue-500">Sumeya Akmel </a>  | 
+            <a href="https://www.linkedin.com/in/sumeya-akmel-2a3b9b270/" target="_blank" rel="noopener noreferrer" className="text-blue-500">Sumeya Akemel </a>  | 
             <a href="https://www.linkedin.com/in/yohana-mekuria-90607a2ab/" target="_blank" rel="noopener noreferrer" className="text-blue-500">Yohana Mekuria </a>
             </p>
 
